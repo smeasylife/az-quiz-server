63b930e7c07b0bbba57872fdb23bf161897c334c
noturavrigk <ksm92038386@gmail.com>
2025-05-31 15:14:05 +0900

    chore: commit for init

---
+++ src/main/java/wemeet/az_web/AzWebApplication.java
@@ -0,0 +1,13 @@
+package wemeet.az_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wemeet/az_web/AzWebApplicationTests.java
@@ -0,0 +1,13 @@
+package wemeet.az_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
