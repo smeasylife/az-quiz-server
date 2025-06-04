114aa8133b97e11f630bca7491815817988c3bdd
noturavrigk <ksm92038386@gmail.com>
2025-06-04 16:31:37 +0900

    Fix: pictureDto에 Status 필드 추가

--- src/main/java/wemeet/az_web/dto/PictureDto.java
+++ src/main/java/wemeet/az_web/dto/PictureDto.java
@@ -1,9 +1,11 @@
 package wemeet.az_web.dto;
 
 import lombok.AllArgsConstructor;
+import wemeet.az_web.enums.Status;
 
 @AllArgsConstructor
 public class PictureDto {
     private Long id;
     private String path;
+    private Status status;
 }
--- src/main/java/wemeet/az_web/repository/PictureRepository.java
+++ src/main/java/wemeet/az_web/repository/PictureRepository.java
@@ -8,6 +8,6 @@ import wemeet.az_web.dto.PictureDto;
 import java.util.List;
 
 public interface PictureRepository extends JpaRepository<Picture, Long> {
-    @Query("select new wemeet.az_web.dto.PictureDto(p.id, p.path) from Picture p where p.section = :section")
+    @Query("select new wemeet.az_web.dto.PictureDto(p.id, p.path, p.status) from Picture p where p.section = :section")
     List<PictureDto> findAllPictureBySection(Integer section);
 }
